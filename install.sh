d5a6b5f796b983b56a6931a18bcf29bffa2eb5eb
kretov <ilya.kretov@gmail.com>
2025-02-11 10:51:37 +0400

    working version

--- install.sh
+++ install.sh
@@ -1,16 +1,33 @@
-curl https://downloader.cursor.sh/linux/appImage/x64 -o cursor.appimage
+if ! dpkg -l | grep -q libfuse2; then
+    sudo apt-get update
+    sudo apt-get install -y libfuse2
+fi
 
-curl https://downloader.cursor.sh/linux/appImage/x64.png -o cursor.png
+curl -L https://downloader.cursor.sh/linux/appImage/x64 -o cursor.appimage
+
+curl https://lobehub.com/e8ce31d9-52ed-444a-9d4c-31771500e1e4 -o cursor.png
 
 chmod +x ./cursor.appimage
 
-mv ./cursor.appimage /opt/cursor.appimage
+mkdir /opt/cursor
+mv ./cursor.appimage /opt/cursor/cursor.appimage
+mv ./cursor.png /opt/cursor/cursor.png
 
 cat << EOF > /usr/share/applications/cursor.desktop
 [Desktop Entry]
 Name=Cursor
-Exec=/opt/cursor.appimage
-Icon=/opt/cursor.png
+Exec=/opt/cursor/cursor.appimage --no-sandbox
+Icon=/opt/cursor/cursor.png
 Type=Application
 Categories=Development;
 EOF
+
+if [ -f ~/.bashrc ]; then
+    echo "alias cursor='/opt/cursor/cursor.appimage --no-sandbox'" >> ~/.bashrc
+fi
+
+if [ -f ~/.zshrc ]; then
+    echo "alias cursor='/opt/cursor/cursor.appimage --no-sandbox'" >> ~/.zshrc
+fi
+
+# sudo rm /opt/cursor/cursor.appimage && sudo rm /opt/cursor/cursor.png && sudo rmdir /opt/cursor && sudo rm /usr/share/applications/cursor.desktop
